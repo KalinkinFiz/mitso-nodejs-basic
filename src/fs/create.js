4de98d33b3084ec4d489b59d44cb6780f32d1860
Pereverzrv Marat <marat.pereverzrv@gmail.com>
2023-03-06 18:28:48 +0300

    add: added functionality to create.js file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+import * as url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const filePath = path.resolve(dirname, "files", "fresh.txt");
+const content = "I am fresh and young"
 const create = async () => {
-    // Write your code here 
+    try{
+        const files = await fs.readdir(path.resolve(dirname, "files"));
+        if(files.includes("fresh.txt")) throw new Error("FS operation failed");
+        await fs.writeFile(filePath, content, {
+            flag: "wx"
+        });
+        console.log("The file has been successfully created, the information has been entered");
+    }
+    catch(err){
+        console.log(err.message);
+    }
 };
 
 await create();
