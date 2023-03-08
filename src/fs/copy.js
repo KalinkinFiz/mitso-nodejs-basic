8c7a451dd1acff93054aed5aa927dcee90c10313
Pereverzrv Marat <marat.pereverzrv@gmail.com>
2023-03-08 20:55:14 +0300

    add: added functionality to copy.js file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+    try{
+        await fs.cp(`${path.resolve(dirname, 'files')}`,`${path.resolve(dirname, 'files_copy')}`,{
+            recursive : true,
+            force: false,
+            errorOnExist : true
+        });
+        console.log('Files have successfully been copied')
+    }
+    catch(err){
+        console.log('FS operation failed');
+    }
 };
 
 await copy();
