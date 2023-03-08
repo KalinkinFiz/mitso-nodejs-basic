20714c059db9dbb3f5ba627d5e3b362a37c2fba4
Pereverzrv Marat <marat.pereverzrv@gmail.com>
2023-03-08 21:19:09 +0300

    add: added functionality to delete.js file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+    
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    
 const remove = async () => {
-    // Write your code here 
+    try{
+        await fs.rm(path.resolve(dirname,'files','fileToRemove.txt'), {}, (err) => {
+            if(err) throw err;
+            console.log('Removing completed');
+        });
+    }
+    catch(err){
+        console.log("FS operation failed")
+    }
 };
 
 await remove();
