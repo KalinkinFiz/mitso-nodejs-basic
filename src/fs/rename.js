657f2b87cb821ca4978e669768f0ee8fbb8b4558
Pereverzrv Marat <marat.pereverzrv@gmail.com>
2023-03-08 21:15:14 +0300

    add: added functionality to rename.js file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+    
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    
 const rename = async () => {
-    // Write your code here 
+    try{
+        await fs.rename(path.resolve(dirname,'files','wrongFilename.txt'), path.resolve(dirname,'files','properFilename.md'), (err) => {
+            if(err) throw err;
+            console.log('Rename completed');
+        });
+    }
+    catch(err){
+        console.log("FS operation failed")
+    }
 };
 
 await rename();
