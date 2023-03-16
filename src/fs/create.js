55bad55acc954da36ea2b722d35705a0491cdaa4
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 18:44:13 +0300

    Prettier

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,13 +3,13 @@ import * as path from "path";
 import * as process from "process";
 
 const create = async () => {
-    let str = "I am fresh and young";
-    let tempPath = path.join(process.cwd(), "src", "fs", "files", "fresh.txt");
-    fs.writeFile(tempPath, str, (err) => {
-        if (err){
-            throw new Error("FS operation failed");
-        }
-    })
+  let str = "I am fresh and young";
+  let tempPath = path.join(process.cwd(), "src", "fs", "files", "fresh.txt");
+  fs.writeFile(tempPath, str, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
