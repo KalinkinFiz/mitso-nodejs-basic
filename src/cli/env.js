c9613bc0226ab3606725608061196ab7e50a21e6
laalcaking <potolock13@gmail.com>
2023-03-16 22:21:13 +0300

    all

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+
+    const prefix = 'MITSO_';
+    const envVariables = process.env;
+    const mitsoVariables = Object.keys(envVariables)
+      .filter(key => key.startsWith(prefix))
+      .map(key => `${key}=${envVariables[key]}`)
+      .join('; ');
+    
+    console.log(mitsoVariables);
+    
 };
 
 parseEnv();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises'
+import path from 'path';
+import url from 'url';
+import crypto from 'crypto'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const file = fs.readFile(path.join(__dirname,'files','fileToCalculateHashFor.txt'))
+
+    const fileData = fs.readFile(path.join(__dirname,(await file).toString()));
+
+    const hash = crypto.createHash('sha256').update(fileData.toString()).digest('hex');
+    console.log(`Hash for ${hash}`)
+    
 };
 
 await calculateHash();
