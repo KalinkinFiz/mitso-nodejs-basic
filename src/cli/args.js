a0dff39b7532673288f458c75dbe7e014843e014
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 21:02:11 +0300

    The realisation of parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import * as process from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+  let obj = new Object();
+  process.argv.slice(2).map((val, index, array) => {
+    if (array[index].startsWith("--")) {
+      obj[val];
+    }
+    if (!val.startsWith("-")) {
+      obj[array[index - 1]] = val;
+    }
+  });
+  for (let item in obj) {
+    console.log(item + " is " + obj[item]);
+  }
 };
 
 parseArgs();
