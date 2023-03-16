ff32989c36ef8d51c760e6cd0dd8148967a75eeb
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 19:00:42 +0300

    The realisation of rename.js function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as process from "process";
+
 const rename = async () => {
-    // Write your code here 
+  let tempPath = path.join(process.cwd(), "src", "fs", "files");
+  if (fs.existsSync(path.join(tempPath, "properFilename.md"))) {
+    throw new Error("FS operation failed");
+  }
+  fs.rename(
+    path.join(tempPath, "wrongFilename.txt"),
+    path.join(tempPath, "properFilename.md"),
+    (err) => {
+      if (err) {
+        throw new Error("Something is wrong");
+      }
+    }
+  );
 };
 
 await rename();
