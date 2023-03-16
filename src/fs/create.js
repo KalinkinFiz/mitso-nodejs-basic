77bb2d8a41bf165ce6f36733e1a45b72862bb3a4
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 18:16:45 +0300

    The realisation of createFile function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as process from "process";
+
 const create = async () => {
-    // Write your code here 
+    let str = "I am fresh and young";
+    let tempPath = path.join(process.cwd(), "src", "fs", "files", "fresh.txt");
+    fs.writeFile(tempPath, str, (err) => {
+        if (err){
+            throw new Error("FS operation failed");
+        }
+    })
 };
 
 await create();
