4e52f2757750b7756c158a51ab8263781578742d
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 22:09:02 +0300

    The realisation of calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import * as crypto from "crypto";
+import * as path from "path";
+import * as process from "process";
+import * as fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  let tempPath = path.join(process.cwd(), "src", "hash", "files");
+  const hash = crypto.createHash("SHA256");
+  const readable = fs.createReadStream(
+    path.join(tempPath, "fileToCalculateHashFor.txt")
+  );
+  readable.on("readable", () => {
+    const data = readable.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
