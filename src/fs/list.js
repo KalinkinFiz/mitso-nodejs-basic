e3ae56f13d68d8e18234d1d62a4134157a044c11
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 20:22:54 +0300

    The realisation of list.js function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import * as fs from "fs";
+import * as path from "path";
+
 const list = async () => {
-    // Write your code here 
+  let tempPath = path.join(process.cwd(), "src", "fs", "files");
+  if (!fs.existsSync(tempPath)) {
+    throw new Error("FS operation failed");
+  }
+  fs.readdir(tempPath, (err, items) => {
+    if (err) {
+      throw new Error("Something is wrong");
+    }
+    items.map((val) => {
+      console.log(val);
+    });
+  });
 };
 
 await list();
