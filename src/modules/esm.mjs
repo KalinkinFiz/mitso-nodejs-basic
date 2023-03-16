23ad0d22d0903a91abf8d972d7454be043107bac
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 21:37:50 +0300

    The reorganization of CommonJS (P.S. Transfer to ECMA Modules (.mjs) CHANGED)

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,26 +1,30 @@
 import * as path from "path";
 import { release, version } from "os";
-import { createServer, createServerHttp } from "http";
-import("./files/c");
+import { createServer } from "http";
+import("./files/c.js");
+import { URL } from "url";
+
+const __filename = new URL("", import.meta.url).pathname;
+const __dirname = new URL(".", import.meta.url).pathname;
 
 const random = Math.random();
 
 export let unknownObject;
 
 if (random > 0.5) {
-  unknownObject = import("./files/a.json");
+  unknownObject = import("./files/a.json", { assert: { type: "json" } });
 } else {
-  unknownObject = import("./files/b.json");
+  unknownObject = import("./files/b.json", { assert: { type: "json" } });
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${path.__filename}`);
-console.log(`Path to current directory is ${path.__dirname}`);
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
 
-export const myServer = createServerHttp((_, res) => {
+export const myServer = createServer((_, res) => {
   res.end("Request accepted");
 });
 
