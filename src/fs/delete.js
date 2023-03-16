1e8fd8e86e8d4f0cabd85c792ab867ed180f1d2f
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 19:12:15 +0300

    The realisation of delete.js function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as process from "process";
+
 const remove = async () => {
-    // Write your code here 
+  let tempPath = path.join(process.cwd(), "src", "fs", "files");
+  if (!fs.existsSync(path.join(tempPath, "fileToRemove.txt"))) {
+    throw new Error("FS operation failed");
+  }
+  fs.unlink(path.join(tempPath, "fileToRemove.txt"), (err) => {
+    if (err) {
+      throw new Error("Something is wrong");
+    }
+  });
 };
 
 await remove();
