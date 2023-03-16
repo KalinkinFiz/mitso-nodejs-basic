cfefe25a64193e62491fd4becc591d7c1e321fb6
Ilya <ilya.sereda.2004@mail.ru>
2023-03-16 22:43:27 +0300

    The realisation of Writable Stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as process from "process";
+import * as fs from "fs";
+import * as path from "path";
+
 const write = async () => {
-    // Write your code here 
+  let tempPath = path.join(process.cwd(), "src", "streams", "files");
+  const streamWritable = fs.createWriteStream(
+    path.join(tempPath, "fileToWrite.txt"),
+    "utf-8"
+  );
+  process.stdin.on("data", (data) => {
+    streamWritable.write(data);
+    process.exit();
+  });
 };
 
 await write();
