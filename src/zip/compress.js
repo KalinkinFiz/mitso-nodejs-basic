0368e91087b73d54151a2d839d79a8daf4f2d042
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 17:11:08 +0300

    The realisation of compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "fs";
+import { join } from "path";
+import { fileURLToPath, URL } from "url";
+import { createGzip } from "zlib";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const streamReadable = createReadStream(
+    join(__dirname, "files", "fileToCompress.txt")
+  );
+  const streamWritable = createWriteStream(
+    join(__dirname, "files", "archive.gz")
+  );
+  streamReadable.pipe(createGzip()).pipe(streamWritable);
 };
 
 await compress();
