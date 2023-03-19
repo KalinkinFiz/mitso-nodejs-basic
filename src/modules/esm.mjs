6f9c4d3f5fcbffdde84b4122ffb590fa8c58fd9f
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 16:54:02 +0300

    The refactoring of esm.mjs file

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,25 +1,28 @@
-import * as path from "path";
+import { sep } from "path";
 import { release, version } from "os";
 import { createServer } from "http";
-import("./files/c.js");
-import { URL } from "url";
+import { URL, fileURLToPath } from "url";
 
-const __filename = new URL("", import.meta.url).pathname;
-const __dirname = new URL(".", import.meta.url).pathname;
+import("./files/c.js");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const random = Math.random();
 
 export let unknownObject;
 
 if (random > 0.5) {
-  unknownObject = import("./files/a.json", { assert: { type: "json" } });
+  let a_json = import("./files/a.json", { assert: { type: "json" } });
+  unknownObject = a_json;
 } else {
-  unknownObject = import("./files/b.json", { assert: { type: "json" } });
+  let b_json = import("./files/b.json", { assert: { type: "json" } });
+  unknownObject = b_json;
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path segment separator is "${sep}"`);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
