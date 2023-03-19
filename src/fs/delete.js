3d290909121f69438d266eb9bd1f47a3c453a6fa
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 15:55:10 +0300

    The refactoring of the delete function 2

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,12 +2,13 @@ import { readdir, unlink } from "fs/promises";
 import { join } from "path";
 import { fileURLToPath, URL } from "url";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const remove = async () => {
   const dirContent = await readdir(join(__dirname, "files"));
-  if (!dirContent.includes("fileToRemove.txt")) throw new Error("FS operation failed");
+  if (!dirContent.includes("fileToRemove.txt"))
+    throw new Error("FS operation failed");
   await unlink(join(__dirname, "files", "fileToRemove.txt"));
-}
+};
 
 await remove();
