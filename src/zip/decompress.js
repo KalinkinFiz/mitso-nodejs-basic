5a9c8cdbb1a00a2c279984c01ed99d6da2181eee
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 17:11:15 +0300

    The realisation of decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "fs";
+import { join } from "path";
+import { fileURLToPath, URL } from "url";
+import { createUnzip } from "zlib";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const streamReadable = createReadStream(
+    join(__dirname, "files", "archive.gz")
+  );
+  const streamWritable = createWriteStream(
+    join(__dirname, "files", "fileToCompress.txt")
+  );
+  streamReadable.pipe(createUnzip()).pipe(streamWritable);
 };
 
 await decompress();
