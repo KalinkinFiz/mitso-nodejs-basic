5befe97b79ecfd4da61ed498772b93599f5bdf38
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 22:09:11 +0300

    The realisation of child process (cp.js file)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { join } from "path";
+import { fileURLToPath, URL } from "url";
+import { fork } from "child_process";
+import { stdin } from "process";
 
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+const spawnChildProcess = async (args) => {
+  const forked = fork(join(__dirname, "files", "script.js"), args, {
+    stdio: [0, 1, 2, "ipc"],
+  });
+  forked.on("message", (msg) => {
+    console.log(msg.trim());
+  });
+  forked.on("disconnect", () => {
+    process.exit(0);
+  });
+  stdin.on("data", (data) => {
+    forked.send(data.toString());
+  });
+};
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Maksim", "Aleksandrovich", "Kalinin"]);
