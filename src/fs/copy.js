cc7b94ce76802f16b6c1ba0822cbe059f4d46640
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 15:54:59 +0300

    The refactoring of the copy function 3

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,15 +2,17 @@ import { cp, mkdir, readdir } from "fs/promises";
 import { join } from "path";
 import { fileURLToPath, URL } from "url";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const copy = async () => {
   const dirContent = await readdir(__dirname);
-  if (!dirContent.includes("files") || dirContent.includes("files_copy")){
+  if (!dirContent.includes("files") || dirContent.includes("files_copy")) {
     throw new Error("FS operation failed");
   }
   await mkdir(join(__dirname, "files_copy"));
-  await cp(join(__dirname, "files"), join(__dirname, "files_copy"), { recursive: true });
+  await cp(join(__dirname, "files"), join(__dirname, "files_copy"), {
+    recursive: true,
+  });
 };
 
 await copy();
