dcaddfab3008d2b745d9abfe7c6738795b79008e
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 20:08:45 +0300

    The realisation of main.js file where the main logic of the workers is

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+import { fileURLToPath, URL } from "url";
+import { join } from "path";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+const workers = [];
+let cores = 0;
+
 const performCalculations = async () => {
-    // Write your code here
+  workers.push(
+    new Promise((resolve, reject) => {
+      const worker = new Worker(join(__dirname, "worker.js"), {
+        workerData: 10 + cores,
+      });
+      worker.on("error", () => {
+        reject({ status: "error", data: null });
+      });
+      worker.on("message", (msg) => {
+        resolve({ status: "resolved", data: msg });
+      });
+    })
+  );
+  if (cores < cpus().length) {
+    cores++;
+    await performCalculations();
+  } else {
+    Promise.allSettled(workers).then((promises) => {
+      promises.forEach((promise) => console.log(promise.value));
+    });
+  }
 };
 
 await performCalculations();
