2db2a34b25d788ad3c03bb82e30dfb890256a051
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 16:46:48 +0300

    The refactoring of the TransformStream function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const streamTransform = new Transform({
+    transform(chunk, _encoding, callback) {
+      callback(null, String(chunk.reverse()).trim() + "\n");
+    },
+  });
+  await pipeline(stdin, streamTransform, stdout);
 };
 
 await transform();
