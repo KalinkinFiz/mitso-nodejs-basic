96bf1da9b76c4b2cd28294ec235a2a4ae67b365e
Ilya <ilya.sereda.2004@mail.ru>
2023-03-19 15:55:29 +0300

    The refactoring of the read function 2

--- src/fs/read.js
+++ src/fs/read.js
@@ -2,12 +2,16 @@ import { readdir, readFile } from "fs/promises";
 import { join } from "path";
 import { fileURLToPath, URL } from "url";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const read = async () => {
   const dirContent = await readdir(join(__dirname, "files"));
-  if (!dirContent.includes("fileToRead.txt")) throw new Error("FS operation failed");
-  const fileContent = await readFile(join(__dirname, "files", "fileToRead.txt"), "utf-8");
+  if (!dirContent.includes("fileToRead.txt"))
+    throw new Error("FS operation failed");
+  const fileContent = await readFile(
+    join(__dirname, "files", "fileToRead.txt"),
+    "utf-8"
+  );
   console.log(fileContent);
 };
 
