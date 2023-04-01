f181142025decf5ebe5d5820dddf9c66d7bac036
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-01 18:38:18 +0300

    env.js ready

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env
+    const find = /MITSO_*/
+    const keys = Object.getOwnPropertyNames(env).filter(name => name.match(find))
+    keys.forEach(key => console.log(key+"="+env[key]))
 };
 
 parseEnv();
