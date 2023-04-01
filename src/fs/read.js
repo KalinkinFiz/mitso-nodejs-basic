493c73eee59c029e0ec8da1b497c22ddbe8d7116
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-01 16:05:51 +0300

    read.js ready

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import {readFile} from 'fs/promises'
+
 const read = async () => {
-    // Write your code here 
+    const file = "files/fileToRead.txt"
+    try {
+        const content = await readFile(file, {encoding: 'utf-8'})
+        console.log(content)
+    } catch (e) {
+        console.error("FS operation failed")
+    }
 };
 
 await read();
