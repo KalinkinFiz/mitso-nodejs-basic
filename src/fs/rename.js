f71020f6f5115fab141f140a5059a3774b44ca50
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-01 15:21:22 +0300

    rename.js ready

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs'
+
 const rename = async () => {
-    // Write your code here 
+    const src = "files/wrongFilename.txt"
+    const dest = "files/properFilename.md"
+    // const src = "files/properFilename.md"
+    // const dest = "files/wrongFilename.txt"
+    try {
+        await fs.promises.access(src)
+        try {
+            await fs.promises.access(dest)
+            console.error("FS operation failed")
+        } catch (e) {
+            await fs.promises.rename(src, dest)
+        }
+    } catch (e) {
+        console.error("FS operation failed")
+    }
 };
 
 await rename();
