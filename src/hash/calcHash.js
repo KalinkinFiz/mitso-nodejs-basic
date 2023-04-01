d216d02db99e555cb44dd55fcf0c93d4f1b62db8
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-01 21:56:27 +0300

    calcHash.js ready

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
 const calculateHash = async () => {
-    // Write your code here 
+    const { readFile } = await import('node:fs/promises');
+    const { createHmac } = await import('node:crypto');
+    const content = await readFile("files/fileToCalculateHashFor.txt", {encoding: 'utf-8'})
+    const secret = 'myStrongSecret';
+    const hash = createHmac('sha256', secret)
+        .update(content)
+        .digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
