e29b2b1e6357cc7fcb45a854fb573c4f96e07b9d
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-01 23:40:03 +0300

    write.js ready

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,9 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+    const fileStream = createWriteStream('files/fileToWrite.txt');
+    await pipeline(process.stdin, fileStream);
 };
 
 await write();
