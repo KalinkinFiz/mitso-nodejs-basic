a74bb460e8f8ef0922e13b9453968ad809990350
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-01 23:17:46 +0300

    read.js ready

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
 const read = async () => {
-    // Write your code here 
+    const { createReadStream } = await import('node:fs');
+    const stream = createReadStream('files/fileToRead.txt');
+
+    stream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
