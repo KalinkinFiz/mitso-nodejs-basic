8410db154306982646ad4dd7ba960816ecc136e1
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-09 16:21:11 +0300

    transform.js ready

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            chunk = chunk.slice(0, -1)
+            const reversed = chunk.toString().split('').reverse().join('');
+            callback(null, reversed+ "\n");
+        }
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
+
