e8d54144c0513bc9f2e5a920ecf28d89282528a0
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-09 23:06:38 +0300

    Worker Threads ready

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import {Worker} from "worker_threads"
+import os from 'os'
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = os.cpus().length;
+    const result = []
+    for (let i = 10; i < 10 + numCores; i++) {
+        const worker = new Worker("./worker.js", {
+            workerData: i
+        })
+        worker.on('message', msg => {
+            result[i - 10] = {status: 'resolved', data: msg}
+        })
+        worker.on('error', () => {
+            result[i - 10] = {status: 'error', data: null}
+        })
+    }
+    setTimeout(() => {
+        console.log(result)
+    }, 200)
 };
 
 await performCalculations();
