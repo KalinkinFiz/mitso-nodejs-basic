77ba10d19bc2bc2adf3d18eae87d163c9bb520e3
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-09 18:53:55 +0300

    decompress.js ready

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const archivePath = 'files/archive.gz';
+    const filePath = 'files/fileToCompress.txt';
+
+    const readStream = createReadStream(archivePath);
+    const writeStream = createWriteStream(filePath);
+
+    const unzipStream = createGunzip();
+
+    readStream.pipe(unzipStream).pipe(writeStream);
+
+    readStream.on('error', (err) => console.error('Read error:', err));
+    writeStream.on('error', (err) => console.error('Write error:', err));
+    unzipStream.on('error', (err) => console.error('Unzip error:', err));
+
+    await new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+    });
 };
 
 await decompress();
