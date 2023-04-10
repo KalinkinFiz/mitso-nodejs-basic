88bf91a818e3b58485be2159c801330647306a92
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-10 13:08:15 +0300

    Child Processes ready

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,6 @@
+import {fork} from 'child_process'
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork('files/script.js', args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['arg1', 'arg2'])
