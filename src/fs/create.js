f205ac3149a3f7522159af31d30cb3b4ab24ac85
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-10 11:28:39 +0300

    create.js ready

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import fs from 'fs'
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.open('files/fresh.txt')
+        console.log('FS operation failed')
+    } catch (e) {
+        await fs.promises.writeFile('files/fresh.txt', 'I am fresh and young')
+    }
 };
 
 await create();
