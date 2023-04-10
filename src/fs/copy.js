6953b70028db188c22194bd2fd0970f107df316a
Vlad Korzun <vladzeyn132@gmail.com>
2023-04-10 11:39:20 +0300

    copy.js ready

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import fs from 'fs'
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.opendir('files_copy')
+        console.error('FS operation failed')
+    } catch (e) {
+        try {
+            await fs.promises.mkdir('files_copy')
+            await fs.promises.cp('files', 'files_copy', { recursive: true })
+        } catch (e) {
+            console.error('FS operation failed')
+        }
+    }
 };
 
 await copy();
