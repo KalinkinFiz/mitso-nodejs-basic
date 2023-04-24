16d47bfd7b1095689a18c4e2ad502d13eee49729
beverlyim <krim.leons@gmail.com>
2023-04-24 21:34:17 +0300

    Добавляю rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { promises } from 'fs';
 
+const rename = async () => {
+    try {
+        const source = './files/wrongFilename.txt';
+        const dest = './files/properFilename.md';
+        const sourceExists = await promises.access(source).then(() => true).catch(() => false);
+        const destExists = await promises.access(dest).then(() => true).catch(() => false);
+        
+        if (sourceExists && destExists) {
+          throw new Error('FS operation failed: Both source and destination files already exist.');
+        } else if (sourceExists) {
+          await promises.rename(source, dest);
+          console.log('File successfully renamed.');
+        } else {
+          throw new Error(`FS operation failed: ${source} does not exist.`);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
 await rename();
