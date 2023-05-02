d74f42f22810b2454d270c08d18fe7942a42c11b
beverlyim <krim.leons@gmail.com>
2023-05-02 15:39:32 +0300

    added delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { promises } from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const filePath = './files/fileToRemove.txt';
+
+    // проверяем, существует ли файл
+    const fileExists = await promises.access(filePath).then(() => true).catch(() => false);
+
+    // если файл не существует, выбрасываем ошибку
+    if (!fileExists) {
+      throw new Error(`FS operation failed: ${filePath} does not exist.`);
+    }
+
+    // удаляем файл
+    await promises.unlink(filePath);
+    console.log('File successfully removed.');
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await remove();
