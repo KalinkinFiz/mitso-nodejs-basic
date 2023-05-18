a05de6b07f1b97f20aeabbf975db160d435d6d31
Pavel <surovcev.p@gmail.com>
2023-05-19 00:26:07 +0300

    lab2_surovcov

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'node:child_process'
+import { execArgv } from "node:process";
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    
+    const controller = new AbortController()
+    const child = fork('./cp/files/script.js',args ,{execArgv})
+    child.on('error', (err) => {
+        
+      });
+      controller.abort();
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+
+spawnChildProcess( ['aafsdf', 'asdfasfd']);
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises'
+import path from 'path'
+import url from 'url'
+
+const CONTENT = "I am fresh and young"
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+
 const create = async () => {
-    // Write your code here 
+    try {
+
+        const files = await fs.readdir(path.join(__dirname, "files"));
+
+        if (files.includes("fresh.txt")) throw new Error("file already exists")
+
+        await fs.writeFile(path.join(__dirname, "files", "fresh.txt"), CONTENT, { flag: "wx" })
+
+        console.log("Good");
+
+    } catch (err) {
+
+        console.log(err.message);
+
+    }
+
 };
 
 await create();
