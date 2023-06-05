cc7f139b487e13141b272e155739deca856056f3
beverlyim <krim.leons@gmail.com>
2023-06-05 21:15:27 +0300

    compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream('files/fileToCompress.txt');
+  const writeStream = createWriteStream('files/archive.gz');
+  const gzipStream = createGzip();
+
+  readStream.pipe(gzipStream).pipe(writeStream);
+
+  await new Promise((resolve, reject) => {
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
 };
 
 await compress();
